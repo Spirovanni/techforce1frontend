07226161bd037dc47b9671f4d74f18ba249c1dbe
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-01 18:26:14 -0800

    Setting up the initial routings

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { MainComponent } from './main.component';
 
-
+const routes: Routes = [
+  {path: 'organisations', component: MainComponent}
+];
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
 })
 export class MainModule { }
