114be08e73d3fb97759e8286589cc48f0ae3223e
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-02 22:29:53 -0800

    Added import { AngularFontAwesomeModule } from 'angular-font-awesome'; for Stars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule} from '@angular/common/http';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AuthModule} from './auth/auth.module';
 import { MainModule} from './main/main.module';
 
@@ -21,7 +23,8 @@ const routes: Routes = [
     AuthModule,
     MainModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AngularFontAwesomeModule
   ],
   exports: [
     RouterModule
